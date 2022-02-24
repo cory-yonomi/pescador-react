1d15d607fac2cae7dabf042b7febed3c276536d5
ornery-mouse <cory.sorel@gmail.com>
2022-02-23 22:56:00 -0600

    backburnering mapbox to get server connected and crud working

--- src/components/Home.js
+++ src/components/Home.js
@@ -12,23 +12,7 @@ const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
 
-	const mapContainer = useRef(null);
-	const map = useRef(null);
-	const [lng, setLng] = useState(-70.9);
-	const [lat, setLat] = useState(42.35);
-	const [zoom, setZoom] = useState(9);
-
-	mapboxgl.accessToken = 'pk.eyJ1IjoiY2Jzb3JlbCIsImEiOiJja3p4cmo1aDgwNDFmMm9uMW50NXNuc25uIn0.hf69sRAXXmSEZi3TBKwnxw'
-
-	useEffect(() => {
-		if (map.current) return; // initialize map only once
-		map.current = new mapboxgl.Map({
-		container: mapContainer.current,
-		style: 'mapbox://styles/mapbox/streets-v11',
-		center: [lng, lat],
-		zoom: zoom
-		});
-	});
+	
 
 	return (
 		<Fragment >
@@ -41,9 +25,6 @@ const Home = (props) => {
 					<button><Link to='/sign-in'>Log In</Link></button><button><Link to='/sign-up'>Create Account</Link></button>
 				
 			</Container>
-			<div>
-				<div ref={mapContainer} className="map-container" />
-			</div>
 		</Fragment>
 	)
 }
