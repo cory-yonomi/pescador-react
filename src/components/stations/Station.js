396ac106e20f085faf091d94ae959eee73defd6b
ornery-mouse <cory.sorel@gmail.com>
2022-05-01 23:09:06 -0500

    clicking + on station creates it in db

--- src/components/stations/Station.js
+++ src/components/stations/Station.js
@@ -1,13 +1,31 @@
 import React from 'react'
+import { gql, useQuery, useMutation } from '@apollo/client'
 import classes from './Stations.module.css'
 
-const Station = ({ station }) => {
+const Station = ({ water, station }) => {
     // ****************** GRAPHQL *********************
-    
+    const CREATE_STATION = gql`
+      mutation AddWaterStation($usgsId: String!, $name: String!, $long: Float!, $lat: Float!){
+        createStation(name: $name, usgsId: $usgsId, long: $long, lat: $lat){
+          name
+          usgsId
+          long
+          lat
+        }
+      }
+    `
+
+    const [createStation] = useMutation(CREATE_STATION, {
+      variables: {
+        name: station.siteName,
+        usgsId: station.siteId,
+        long: station.siteLong,
+        lat: station.siteLat
+      }
+    })
 
     const clickHandler = e => {
-        
-        
+        createStation()        
     }
 
     //borrowed title casing function(Greg Dean on StackOverflow)
